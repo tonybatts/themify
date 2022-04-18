bab0e57e18f3460b82d5224e658362d12d02d32b
Tony Batts <tony.batts@knfilters.com>
2022-04-17 17:28:04 -0700

    Add shortcuts

--- background.js
+++ background.js
@@ -1,9 +1,23 @@
-// chrome.browserAction.onClicked.addListener((tab) => {
-//   chrome.tabs.query({ active: true, currentWindow: true },
-//     () => {
-//       chrome.tabs.executeScript({
-//         file: "themizer.js"
-//       })
-//     }
-//   )
-// })
+chrome.commands.onCommand.addListener((command) => {
+  switch (command) {
+    case "eye-dropper":
+      activateEyeDropper();
+      break;
+    default:
+      console.log(`Command ${command} not found`);
+  }
+});
+
+const activateEyeDropper = () => {
+  chrome.tabs.executeScript({
+    file: "themizer.js"
+  });
+
+  setTimeout(() => {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tab) => {
+      chrome.tabs.sendMessage(tab[0].id, "eyedropper", (response) => {
+        console.log(response);
+      });
+    });
+  }, 500);
+};
