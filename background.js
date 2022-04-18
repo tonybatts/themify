65041466feb3deb616348067f37be29292631298
Tony Batts <tonybatts@gmail.com>
2022-04-17 17:38:31 -0700

    Added background.js
    Background.js listens for shortcuts

---
+++ background.js
@@ -0,0 +1,23 @@
+chrome.commands.onCommand.addListener((command) => {
+  switch (command) {
+    case "eye-dropper":
+      activateEyeDropper();
+      break;
+    default:
+      console.log(`Command ${command} not found`);
+  }
+});
+
+const activateEyeDropper = () => {
+  chrome.tabs.executeScript({
+    file: "themizer.js"
+  });
+
+  setTimeout(() => {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tab) => {
+      chrome.tabs.sendMessage(tab[0].id, "eyedropper", (response) => {
+        console.log(response);
+      });
+    });
+  }, 500);
+};
