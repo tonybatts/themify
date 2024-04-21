8ae470927d82c4ee82fd8134ba4e0e2309e57d79
Tony Batts <tonybatts@gmail.com>
2024-04-21 15:32:47 -0700

    Update themizer.js
    clean up

--- themizer.js
+++ themizer.js
@@ -1,81 +1,64 @@
-colorArr = [];
+// queries all of the elements on the page and returns an array of color values
+const getAllElementColors = () => {
+  const themifyColorArray = [];
 
-elArr = [
-  ...document.querySelectorAll(
-    "a, p, div, section, aside, span, h1, h2, h3, h4, h5, h6, body, footer, nav, article, main, blockquote, button"
-  )
-];
-elArr.forEach((el) => {
-  let styles = window.getComputedStyle(el);
+  const themifyElementArray = [
+    ...document.querySelectorAll(
+      "a, p, div, section, aside, span, h1, h2, h3, h4, h5, h6, body, footer, nav, article, main, blockquote, button"
+    )
+  ];
+  themifyElementArray.forEach((el) => {
+    let styles = window.getComputedStyle(el);
 
-  if (styles.backgroundColor && !colorArr.includes(styles.backgroundColor)) {
-    colorArr.push(styles.backgroundColor);
-  }
-
-  if (styles.color && !colorArr.includes(styles.color)) {
-    colorArr.push(styles.color);
-  }
-});
-
-if (!document.querySelector(".themify_2349_idfja")) {
-  const themifyCheck = document.createElement("span");
-  themifyCheck.classList.add("themify_2349_idfja");
-  document.body.appendChild(themifyCheck);
-
-  const copyToClipboard = (str) => {
-    const el = document.createElement("textarea");
-    el.classList.add("themizer_copy");
-    el.value = str;
-    el.setAttribute("readonly", "");
-    el.style.position = "absolute";
-    el.style.left = "-9999px";
-    document.body.appendChild(el);
-    el.select();
-    // document.execCommand("copy");
-
-    navigator.clipboard
-      .writeText(el.value)
-      .then(() => {
-        console.log("Color copied to clipboard...");
-      })
-      .catch((err) => {
-        console.log("Something went wrong", err);
-      });
-
-    document.body.removeChild(el);
-  };
-
-  const eyeDropper = (sendResponse) => {
-    if (!window.EyeDropper) {
-      console.log("Your browser does not support the EyeDropper API");
-      return;
+    if (styles.backgroundColor && !themifyColorArray.includes(styles.backgroundColor)) {
+      themifyColorArray.push(styles.backgroundColor);
     }
 
-    const eyeDropper = new EyeDropper();
-
-    eyeDropper
-      .open()
-      .then((result) => {
-        copyToClipboard(result.sRGBHex);
-        sendResponse("hello");
-      })
-      .catch((e) => {
-        console.log("error", e);
-      });
-  };
-
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message === "eyedropper") {
-      window.focus();
-      eyeDropper(sendResponse);
+    if (styles.color && !themifyColorArray.includes(styles.color)) {
+      themifyColorArray.push(styles.color);
     }
-
-    return true;
   });
-}
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message === "ready") {
-    sendResponse(JSON.stringify(colorArr));
+  return themifyColorArray;
+};
+
+const copyToClipboard = (str) => {
+  const el = document.createElement("textarea");
+  el.value = str;
+  el.setAttribute("readonly", "");
+  el.style.position = "absolute";
+  el.style.left = "-9999px";
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+};
+
+const eyeDropper = async () => {
+  const eyeDropper = new EyeDropper();
+  const abortController = new AbortController();
+
+  try {
+    const result = await eyeDropper.open({ signal: abortController.signal });
+    const colorHexValue = result.sRGBHex;
+    if (colorHexValue) {
+      copyToClipboard(result.sRGBHex);
+    }
+  } catch (e) {
+    console.log(e);
+  } finally {
+    abortController.abort();
+  }
+};
+
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  if (message === "colors") {
+    const themifyPageColors = getAllElementColors();
+    sendResponse(JSON.stringify(themifyPageColors));
+  }
+
+  if (message === "eyedropper") {
+    await eyeDropper();
+    sendResponse("copied");
   }
 });
